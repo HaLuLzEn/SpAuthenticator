c1057fd5ea24019d358374686300dd1557a4977e
HaLuLzEn <kinghalil667@gmail.com>
2024-12-10 04:23:04 +0100

    removed 2fa relation table from the database update script

--- src/main/resources/UptadeDB.sql
+++ src/main/resources/UptadeDB.sql
@@ -12,11 +12,4 @@ CREATE TABLE IF NOT EXISTS users (
     CONSTRAINT PRIMARY KEY pk_user_id USING BTREE (id)
 );
 
-CREATE TABLE IF NOT EXISTS user_2fa_rel (
-    id VARCHAR(255) PRIMARY KEY NOT NULL,
-    fk_user_id VARCHAR(255) NOT NULL,
-    2fa_key VARCHAR(255) NOT NULL,
-    CONSTRAINT FOREIGN KEY (fk_user_id) REFERENCES users(id)
-);
-
 INSERT INTO users (id, email, username) VALUES ('test', 'test', 'Basement man');
